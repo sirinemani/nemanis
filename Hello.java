9823e78db50c3f8dec95802b738ba3c64caa79e0
sirinemani <84816862+sirinemani@users.noreply.github.com>
2021-05-25 18:34:20 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**********************************************
+*Hello.java
+***********************************************
+*Srinidhi Nemani
+*5/25/2021
+*CMSC 255
+************************************************/
+public class Hello{
+	public static void main(String[] args) {
+	
+		System.out.println("Srinidhi Nemani");
+		System.out.println("Hello.java");
+		System.out.println("This program prints stuff");
+		System.out.println("5/25/2021");
+		System.out.println("CMSC 225");
+		System.out.println();
+	}
+}
+		
